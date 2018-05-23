705136e969207a521a658f4aa3d6fd0d56ab4246
Alexander Rieder <alexander.rieder@re-host.eu>
2018-05-23 16:25:02 +0200

    UhBaby

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,5 @@
 import React,  { Component }  from 'react';
-import { StyleSheet, Text, View, AppRegistry, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, Image, Button, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class LogoTitle extends React.Component {
@@ -27,22 +27,85 @@ class HomeScreen extends React.Component {
   };
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
-              itemId: 86,
-              otherParam: 'anything you want here',
-            });
-          }}
+      
+      <View style={styles.container}>
+       <Image
+          style={{width: '100%', height: '50%'}}
+          source={require('./karsau.png')}
+        />
+        <FlatList
+          data={[
+            {key: 'Über Karsau', togo: 'Static', content: 'History'},
+            {key: 'Kontakt', togo: 'Static', content: 'Contact'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <TouchableOpacity onPress={() =>
+            this.props.navigation.push(item.togo, {
+              itemId: '33', title: item.key, content: item.content},
+            )}
+            ><Text style={styles.item}>{item.key}</Text></TouchableOpacity>}
         />
       </View>
     );
   }
 }
+
+class Contact extends React.Component{
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={styles.contacttext}>Ortsvorsteher</Text>
+        <Text style={styles.contacttext}>Herr Jürgen Räuber</Text>
+        <Text style={styles.contacttext}>j.raeuber@rheinfelden-baden.de</Text>
+        <Text style={styles.contacttext}>Tel.:    07623 - 5151</Text>
+        <Text style={{paddingVertical: 20}}></Text>
+<Text style={styles.contacttext}>Webmaster</Text>
+<Text style={styles.contacttext}>Herr Herbert Herzog</Text>
+<Text style={styles.contacttext}>webmaster@karsau-750.de</Text>
+<Text style={styles.contacttext}>Tel.: +49 7623 50991</Text>
+<Text style={styles.contacttext}>Mobil: +49 1520 17 96 007</Text>
+</View>
+    );
+  }
+}
+
+class History extends React.Component{
+  render(){
+    return(
+      <ScrollView>
+    <Text style={styles.longtext}>Karsau bildete mit Riedmatt und Beuggen einst ein geschlossenes Territorium
+    der 1246 gegründeten Deutschordenskommende Beuggen. Beuggen wird
+    erstmals 1218 als „Bugheim“ erwähnt,
+    und im Jahr 1269 folgen die urkundlichen Nennungen von Karsau als „Karlesouwe“
+    und von Riedmatt als „zu Rietmatten“.
+    
+    Karsau selbst liegt am südlichen Hang des Dinkelberges. Riedmatt dagegen
+    in einer zum Rhein hin verlaufenden Talmulde. Westlich davon schließt
+    sich Beuggen mit seiner Mitte des 13. Jahrhunderts erbauten, mächtigen Burganlage
+    des Deutschordenschlosses an.
+    Die Grundherren hatten die Grund- und Leibherrschaft, die niedere und seit 1739 auch
+    die hohe Gerichtsbarkeit über die Untertanen.
+    Der Komtur Beuggens war Mitglied des Prälatenstandes in Vorderösterreich.</Text>
+    <View style={{padding: 15}}>
+    <View>
+    <Image 
+    style={{width: '100%'}}
+    source={require('./postkarte.jpg')}
+    />
+    <Text style={{paddingTop: 5}}>Karsauer Ansichtskarte um 1915</Text>
+    </View>
+    </View>
+    <Text style={styles.longtext}>Im Jahr 1806 kam die Deutschordenskommende in den Besitz des Großherzogs von Baden. Die Schlossanlage wurde im Lauf der Jahrhunderte verschiedentlich genutzt und wechselte auch mehrmals den Besitzer. Ab 1954 gehörte sie der evangelischen Landeskirche und wurde zuletzt als Tagungsstätte genutzt. Ende 2016 wechselte wiederum der Besitzer.</Text>
+    </ScrollView>
+    );
+  }
+}
+
 
 class DetailsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -87,10 +150,48 @@ class DetailsScreen extends React.Component {
   }
 }
 
+class StaticTextScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state;
+    
+    return {
+      title: params ? params.title : 'Static Text Screen',
+    }
+  };
+  render() {
+    /* 2. Get the param, provide a fallback value if not available */
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
+    const displayContent = navigation.getParam('content', );
+    switch (displayContent) {
+      case 'History':
+      return(
+        <History/>
+      );
+      case 'Contact':
+      return(
+        <Contact/>
+      )
+
+
+
+      default:
+        return (
+          <View style={styles.lvl2}>
+            <Text>Null</Text>
+          </View>
+          );
+    }
+    
+  }
+}
+
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     Details: DetailsScreen,
+    Static: StaticTextScreen,
   },
   {
     initialRouteName: 'Home',
@@ -105,6 +206,26 @@ const RootStack = createStackNavigator(
     },
   }
 );
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 0
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  longtext: {
+    padding: 10,
+    fontSize: 18,
+    
+  },
+  contacttext: {
+    fontSize: 18,
+    
+  },
+})
 
 export default class App extends React.Component {
   render() {
